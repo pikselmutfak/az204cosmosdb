194d526fe98fe31717ea9363d98d180581ece403
Kazım Etiksan <kazim@pikselmutfak.com>
2022-03-09 14:06:41 +0300

    update

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://pixcosmos:Nu49V6lN2Wd7jPaFJdph14AyrRqNZ76VdwXlDIrIl7iapKNSHnTuwMAatvGqves0h4LwW3W32WuKvojYojWK4Q==@pixcosmos.mongo.cosmos.azure.com:10255/?ssl=true&appName=@pixcosmos@', {
+// const connStr = process.env.conn_str
+// app service hizmeti
+
+mongoose.connect('', {
     useNewUrlParser: true,
     socketTimeoutMS: 5000,
     useUnifiedTopology: true,
